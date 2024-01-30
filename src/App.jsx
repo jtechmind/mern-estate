ae5cd593cdab3c197fab8fcf0e5da076ae9cb6a8
jtechmind <jtechmind@gmail.com>
2024-01-30 23:56:09 +0530

    Pages and Routes created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/about";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  return <h1 className="text-red-600">Real Estate App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
