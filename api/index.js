10f052b510c650a40ddc99e75a43a9eefdaa4588
jtechmind <jtechmind@gmail.com>
2024-02-01 23:27:37 +0530

    create model, controller and routes for user and test api.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
 dotenv.config();
 const app = express();
 
@@ -16,3 +17,5 @@ mongoose
 app.listen(4100, () => {
   console.log("server is running on port 4100");
 });
+
+app.use("/api/user", userRouter);
