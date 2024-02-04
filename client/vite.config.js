6421f6293f356859fdac62c4eb844e5fbcac2be0
jtechmind <jtechmind@gmail.com>
2024-02-04 23:55:23 +0530

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:4100",
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
